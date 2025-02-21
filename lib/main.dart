6fd46a8b7664807c434df2ddf3244ef652421d87
jishnu6241 <jishnuov2002@gmail.com>
2025-02-21 19:50:41 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:pick_n_pay_shop/features/home/home.dart';
+import 'package:pick_n_pay_shop/theme/app_theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: appTheme,
+      home: const Home(),
+    );
+  }
+}
